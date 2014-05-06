aa6e32e08744a0cb426c0af4d044a384bd523ce3
lfarrell <lfarrell@email.unc.edu>
2014-05-06 16:28:33 -0400

    through page 59

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -1,11 +1,12 @@
 ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbone, Marionette, $, _) {
     List.Contact = Marionette.ItemView.extend({
         template: "#contact-template",
-        tagName: "li"
+        tagName: "tr"
     });
 
     List.Contacts = Marionette.CollectionView.extend({
-        tagName: "ul",
+        tagName: "table",
+        className: "table table-hover",
         itemView: List.Contact
     });
 });
