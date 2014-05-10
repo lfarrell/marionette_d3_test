70ac58ccd83fefd77a04d8c65867d2f143de39ad
Larry Farrell <farrelldlfarrell@gmail.com>
2014-05-10 09:25:30 -0400

    Through page 61

--- assets/js/apps/contacts/list/list_controller.js
+++ assets/js/apps/contacts/list/list_controller.js
@@ -2,8 +2,13 @@ ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbon
    List.Controller = {
        listContacts: function() {
            var contacts = ContactManager.request("contact:entities");
+
            var contactsListView = new List.Contacts({
                 collection: contacts
+           });
+
+           contactsListView.on("itemview:contact:delete", function(childView, model) {
+                contacts.remove(model);
            });
 
            ContactManager.mainRegion.show(contactsListView);
--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -1,7 +1,32 @@
 ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbone, Marionette, $, _) {
     List.Contact = Marionette.ItemView.extend({
         template: "#contact-list-item",
-        tagName: "tr"
+        tagName: "tr",
+
+        events: {
+            "click": "highlightName",
+            "click button.js-delete": "deleteClicked"
+        },
+
+        highlightName: function() {
+            this.$el.toggleClass("warning");
+        },
+
+        deleteClicked: function(e){
+            e.stopPropagation();
+            this.trigger("contact:delete", this.model);
+        },
+
+        remove: function() {
+            var self = this;
+            this.$el.fadeOut(function() {
+               Marionette.ItemView.prototype.remove.call(self);
+            });
+        },
+
+        alertName: function(e) {
+            alert($(e.target).text());
+        }
     });
 
     List.Contacts = Marionette.CompositeView.extend({
